7c0b61ae453268ef5b7c0ebc08a8e1c587db63e2
Матвей Агафонов <113810781+krutoiChel2004@users.noreply.github.com>
2024-09-11 11:41:49 +0300

    fix: ByteToUint16

--- trnsformData.go
+++ trnsformData.go
@@ -5,14 +5,18 @@ import (
 	"math"
 )
 
-func ByteToBool(bytes []byte, id int) bool {
+func ByteToUint16(bytes []byte, id int) uint16 {
 	bytes[0], bytes[1] = bytes[1], bytes[0]
 
 	reg := binary.LittleEndian.Uint16(bytes)
 
 	l := [16]uint16{1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8196, 16364, 32768}
 
-	return reg&l[id] == l[id]
+	if reg&l[id] == l[id] {
+		return 1
+	}
+
+	return 0
 }
 
 func ByteToFloat32(bytes []byte) float32 {
