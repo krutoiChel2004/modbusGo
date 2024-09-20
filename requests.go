f6b270e060bef055216290a5aabe22ab396414b1
Матвей Агафонов <113810781+krutoiChel2004@users.noreply.github.com>
2024-09-20 15:27:11 +0300

    fix: del print response

--- requests.go
+++ requests.go
@@ -12,26 +12,22 @@ func (h *TCPHandler) ReadRegisters(slaveID, functionCode, address, quantity uint
 	request[4] = 0 // Length
 	request[5] = 6
 	request[6] = byte(slaveID)      // Unit ID
-	request[7] = byte(functionCode) // Function Code (0x03 Read Holding Registers)
+	request[7] = byte(functionCode)
 	request[8] = byte(address >> 8)
 	request[9] = byte(address)
 	request[10] = byte(quantity >> 8)
 	request[11] = byte(quantity)
 
-	// Отправка запроса
 	_, err := h.conn.Write(request)
 	if err != nil {
 		return nil, fmt.Errorf("failed to send request: %w", err)
 	}
 
-	// Получение ответа
 	response := make([]byte, 9+2*int(quantity))
 	_, err = h.conn.Read(response)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read response: %w", err)
 	}
 
-	fmt.Println(response)
-	// Проверка и возврат данных
 	return response[9:], nil
 }
